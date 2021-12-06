596ce2f154302d5fe6392ae227ae6e3f652be9ee
Erica Pescio <edro15@users.noreply.github.com>
2021-12-06 10:43:09 +0100

    Minor fixes for automated tests

--- sophosendpointprotection_connector.py
+++ sophosendpointprotection_connector.py
@@ -23,8 +23,8 @@ import re
 import phantom.app as phantom
 import requests
 from bs4 import BeautifulSoup
-from phantom.base_connector import BaseConnector
 from phantom.action_result import ActionResult
+from phantom.base_connector import BaseConnector
 
 from sophosendpointprotection_consts import *
 
